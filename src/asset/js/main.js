857583cef58e5669cc2206e6accb9981b141ffea
mrFlick72 <valerio.vaudi@gmail.com>
2016-09-03 00:16:34 +0200

    revert

--- src/asset/js/main.js
+++ src/asset/js/main.js
@@ -3,9 +3,8 @@
 angular.module('valeriovaudi', ['ui.router', "home", 'resume', "music","cs", "aboutMe","contact", "siteDirectiveModules"])
     .config(["$urlRouterProvider", function($urlRouterProvider){
         $urlRouterProvider.otherwise("/");
-    }]).controller("baseCtr",["$scope", "$window", "i18nPageContentResolver", function($scope, $window, i18nPageContentResolver){
+    }]).controller("baseCtr",["$scope", "i18nPageContentResolver", function($scope, i18nPageContentResolver){
         $scope.avatar="dist/asset/images/myPhoto.jpeg";
-        $scope.showHeader= $window.innerWidth > 1024;
         i18nPageContentResolver.jsonMessagesResolver("dist/asset/data/menuLabel").success(function(data){
             $scope.menuLabel = data;
         });
