8cf1103a51aceee6d7d6c0e5240d090ccf1d43df
mrFlick72 <Back72Star>
2018-08-07 22:12:24 +0200

    contact me page send mail popup confirmation and error

--- app/page/ContactMePage.js
+++ app/page/ContactMePage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import contactMeAvatar from "../asset/images/mail-min.jpg"
 import "../asset/css/components.css"
 import MailRepository from "../domain/repository/MailRepository";
+import $ from "jquery/dist/jquery";
+import "bootstrap/dist/js/bootstrap";
 
 export default class ContactMePage extends React.Component {
 
@@ -13,6 +15,12 @@ export default class ContactMePage extends React.Component {
             mailSubject: React.createRef(),
             mailContent: React.createRef()
         };
+
+        this.state = {
+            alertClass: "alert alert-danger",
+            modalTitle: "",
+            modalMessage: ""
+        }
 
         this.mailRepository = new MailRepository();
         this.sendMail = this.sendMail.bind(this);
@@ -26,6 +34,24 @@ export default class ContactMePage extends React.Component {
             "message": this.mailDataRef.mailContent.current.value
         };
         this.mailRepository.sendMail(mailData)
+            .then(response => {
+                if (response.status === 201) {
+                    this.setState({
+                        alertClass: "alert alert-success",
+                        modalTitle: "Mail has been sent ",
+                        modalMessage: "Your email has been sent correctly"
+                    });
+                } else {
+                    this.setState({
+                        alertClass: "alert alert-danger",
+                        modalTitle: "Mail has been sent ",
+                        modalMessage: "Your email has been not sent correctly"
+                    });
+                    $("#contactModalMessage").modal("show");
+                }
+
+                $("#contactModalMessage").modal("show");
+            })
     }
 
     render() {
@@ -34,8 +60,9 @@ export default class ContactMePage extends React.Component {
                 <div className="row">
                     <div className="col-12">
                         <img src={contactMeAvatar} className="left-decorator"/>
-                        <h3>If you have any question do not hesitate, contact me on <a
-                            href="mailto:valerio.vaudi@gmail.com"></a>valerio.vaudi@gmail.com or using the form below
+                        <h3>If you have any question do not hesitate, contact me on
+                            <a href="mailto:valerio.vaudi@gmail.com"> valerio.vaudi@gmail.com</a> or using the form
+                            below
                         </h3>
                     </div>
                 </div>
@@ -79,6 +106,27 @@ export default class ContactMePage extends React.Component {
                         </div>
                     </div>
 
+                </div>
+
+                <div id="contactModalMessage" className="modal" tabIndex="-1" role="dialog">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">{this.state.modalTitle}</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <div className={this.state.alertClass} role="alert">
+                                    <p>{this.state.modalMessage}</p>
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
