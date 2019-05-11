b09bd54feedb9d1918acd34eb006e6a96a52cb49
mrFlick72 <valerio.vaudi@gmail.com>
2019-05-11 18:39:18 +0200

    blog page

--- app/page/BlogPage.js
+++ app/page/BlogPage.js
@@ -13,18 +13,30 @@ export default class BlogPage extends React.Component {
 
         this.state = {
             blogDocs: [
-                "spring-cloud-kubernetes-demo",
-                "reservation-service",
-                "bootiful-kotlin-todo-list",
-                "sleuth-spike"],
+                {
+                    name: "spring-cloud-kubernetes-demo",
+                    description: "Spring Cloud Kubernetes usage example"
+                },
+                {
+                    name: "reservation-service",
+                    description: "R2DBC usage Example"
+                },
+                {
+                    name: "bootiful-kotlin-todo-list",
+                    description: "Bootiful todo list written in Kotlin"
+                },
+                {
+                    name: "sleuth-spike",
+                    description: "A Sleuth usage example"
+                }],
         }
 
         this.webContentRepository = new WebContentRepository()
     }
 
-    getBlogContent(repoName) {
+    getBlogContent(repoName, description) {
         this.webContentRepository.getBlogContent(repoName)
-            .then(markdownText => this.setState({blogContent: markdownText}))
+            .then(markdownText => this.setState({blogContent: markdownText, blogContentTitle: description}))
     }
 
 
@@ -34,13 +46,13 @@ export default class BlogPage extends React.Component {
             <ul className="list-group">
                 {this.state.blogDocs.map(blogDocName =>
                     <li className="list-group-item"
-                        onClick={this.getBlogContent.bind(this, blogDocName)}>{blogDocName}</li>)}
+                        onClick={this.getBlogContent.bind(this, blogDocName.name, blogDocName.description)}>{blogDocName.description}</li>)}
             </ul>
 
             <RowSeparator/>
 
             {this.state.blogContent &&
-            <MarckDownDocumentReader title="Test" documentText={this.state.blogContent}/>}
+            <MarckDownDocumentReader title={this.state.blogContentTitle} documentText={this.state.blogContent}/>}
         </React.Fragment>
     }
 }
